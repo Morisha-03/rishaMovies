ca9cf79dc323d2cef6dbaf6a4c626d1e3107b201
Morisha-03 <willockmorisha@gmail.com>
2024-02-07 16:17:35 +0300

    Changes on Movies component

--- src/Pages/Movies/Movies.js
+++ src/Pages/Movies/Movies.js
@@ -39,18 +39,17 @@ const Movies = () => {
         setPage={setPage}
       />
       <div className="trending">
-        {content &&
-          content.map((c) => (
-            <SingleContent
-              key={c.id}
-              id={c.id}
-              poster={c.poster_path}
-              title={c.title || c.name}
-              date={c.first_air_date || c.release_date}
-              media_type="movie"
-              vote_average={c.vote_average}
-            />
-          ))}
+        {content?.map((c) => (
+          <SingleContent
+            key={c.id}
+            id={c.id}
+            poster={c.poster_path}
+            title={c.title || c.name}
+            date={c.first_air_date || c.release_date}
+            media_type="movie"
+            vote_average={c.vote_average}
+          />
+        ))}
       </div>
       {numOfPages > 1 && (
         <CustomPagination setPage={setPage} numOfPages={numOfPages} />
